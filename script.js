6412e376d26cc70ebb9a0d4c07fb35a4af5e3602
Neha-Balaji-Bacha <neha170603@gmail.com>
2025-11-28 23:42:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 //your JS code here. If required.
+const changeBtn = document.getElementById("change_button");
+const resetBtn = document.getElementById("Reset");
+
+changeBtn.addEventListener("click", () => {
+  const blockId = document.getElementById("block_id").value;
+  const color = document.getElementById("colour_id").value;
+
+  const items = document.querySelectorAll("#grid-item");
+
+  // make all transparent first
+  items.forEach(item => {
+    item.style.backgroundColor = "transparent";
+  });
+
+  // find the correct box
+  items.forEach(item => {
+    if (item.getAttribute("data-id") == blockId) {
+      item.style.backgroundColor = color;
+    }
+  });
+});
+
+resetBtn.addEventListener("click", () => {
+  const items = document.querySelectorAll("#grid-item");
+  items.forEach(item => {
+    item.style.backgroundColor = "transparent";
+  });
+});
